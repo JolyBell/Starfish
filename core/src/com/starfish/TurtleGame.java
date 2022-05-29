73fd04cb34df870446e1bf38a7500b4490f45717
GoryatFonari <perelygin.ad@edu.spbstu.ru>
2022-05-29 19:06:56 +0300

    Initial commit

---
+++ core/src/com/starfish/TurtleGame.java
@@ -0,0 +1,13 @@
+package com.starfish;
+
+import com.badlogic.gdx.Game;
+public class TurtleGame extends Game
+{
+    public void create()
+    {
+        TurtleLevel tl = new TurtleLevel(this);
+        setScreen( tl );
+    }
+}
+
+
---
+++ desktop/src/com/starfish/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.starfish;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.starfish.Starfish;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1000, 800);
+		config.setTitle("Starfish Collector");
+		new Lwjgl3Application(new TurtleGame(), config);
+	}
+}
